5e060267ae184a037e5e4c040ecca860acf3744d
Joshua Jack Marden <joshuasigma@Joshuas-MBP.lan>
2024-09-09 16:36:04 +0100

    import globals

--- rust_backend/src/ingestion/ons_api.rs
+++ rust_backend/src/ingestion/ons_api.rs
@@ -1,5 +1,33 @@
+// ╔═════════════════════════════ ◈{ Imports }◈ ══════════════════════════════╗
+
+
 use tokio;
 use reqwest;
 use sqlx::postgres::PgPoolOptions;
 use std::env;
 
+// ╔═════════════════════════ ◈{ Import  Globals }◈ ══════════════════════════╗
+
+use serde::Deserialize;
+use std::fs;
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub data_sources: DataSources,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct DataSources {
+    pub api_url: String,
+    pub other_service_url: String,
+}
+
+pub fn load_config() -> Result<Config, Box<dyn std::error::Error>> {
+    // Read the YAML file
+    let config_yaml = fs::read_to_string("global_config.yaml")?;
+    
+    // Parse the YAML file
+    let config: Config = serde_yaml::from_str(&config_yaml)?;
+
+    Ok(config)
+}
